2dd2c25cbe42e97a1fb3db318e51ecd90f3b1ecb
hyukjunkim1116 <nmkk1234@naver.com>
2024-01-21 23:46:03 +0900

    set post api

--- src/services/post.js
+++ src/services/post.js
@@ -1,26 +1,16 @@
-import { getUserById } from './user';
-export async function createPost(data) {
-  return await api.post('api/post/', {
-    title: data.title,
-    content: data.content,
-  });
-}
+import { api } from 'boot/init';
 export async function getPosts() {
-  return await api.post('api/post/');
+  return await api.get('post/');
 }
-export async function getPost(postId, data) {
-  return await api.post(`api/post/${postId}`, {
-    title: data.title,
-    content: data.content,
-  });
+export async function getPost() {
+  return await api.post('post/');
 }
-export async function updatePost(uid, postId, data) {
-  return await api.post(`api/${uid}_${postId}`, {
-    title: data.title,
-    content: data.content,
-  });
+export async function createPost(data) {
+  return await api.post('post/', data);
 }
-
-export async function deletePost(uid, postId, data) {
-  return await api.post(`api/${uid}_${postId}`);
+export async function updatePost(postId, data) {
+  return await api.post(`post/${postId}`, data);
+}
+export async function deletePost(postId) {
+  return await api.post(`post/${postId}`);
 }
