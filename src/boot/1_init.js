26de117e577ce04c53e4a366a9b05457cc663a81
hyukjunkim1116 <nmkk1234@naver.com>
2024-01-27 23:19:45 +0900

    fix axios interceptor

--- src/boot/1_init.js
+++ src/boot/1_init.js
@@ -1,10 +1,8 @@
-import { boot } from 'quasar/wrappers';
 import axios from 'axios';
+import { boot } from 'quasar/wrappers';
 import { useAuthStore } from 'src/stores/auth';
 import { Cookies } from 'quasar';
 import { logout } from 'src/services';
-import { useRouter } from 'vue-router';
-import { useAsyncState } from '@vueuse/core';
 
 const djangoApi = 'http://127.0.0.1:8000';
 const springApi = 'http://localhost:8080';
@@ -48,55 +46,56 @@ export default boot(async ({ app }) => {
   // 전역 axios 인스턴스를 설정하여, 앱의 모든 곳에서 사용 가능하게 함
   app.config.globalProperties.api = await setupApi();
   api.interceptors.request.use(async config => {
+    if (!config.headers) return config;
     const accessToken = Cookies.get('access');
-    if (accessToken) {
+    if (accessToken && config.headers) {
       config.headers.Authorization = `Bearer ${accessToken}`;
     }
     return config;
   });
+  let refreshing = false;
   api.interceptors.response.use(
     response => response,
     async error => {
-      console.log('useerror', error);
+      if (error.config && error.response && error.response.status === 401) {
+        if (!refreshing) {
+          try {
+            refreshing = true;
 
-      const originalRequest = error.config;
-      console.log('originalRequest', originalRequest);
-      // AccessToken이 만료되었을 경우 재발급 후 다시 요청
-      if (error.response.status === 401 && !originalRequest._retry) {
-        originalRequest._retry = true;
-        const refreshToken = Cookies.get('refresh');
-        const { error, state } = useAsyncState(
-          async () => {
+            const originalRequest = error.config;
+            originalRequest._retry = true;
+            const refreshToken = Cookies.get('refresh');
+
+            if (!refreshToken) {
+              throw new Error('Refresh token not found');
+            }
+
             const response = await api.post('users/token/refresh/', {
               refresh: refreshToken,
             });
-            return response;
-          },
-          {},
-          {
-            onSuccess: async response => {
-              try {
-                console.log('state');
-                console.log('response');
-                const authStore = useAuthStore();
-                const newAccessToken = response.data.access;
-                authStore.setUserToken(newAccessToken, refreshToken);
-                originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-                console.log('api(originalRequest)', api(originalRequest));
-                return await api(originalRequest);
-              } catch (err) {
-                await logout();
-                console.log('Register err : ', err.response);
-                alert('다시 로그인 하세용');
-                window.location.replace('/');
-              }
-            },
-            onError: async err => {},
-          },
-        );
+
+            const authStore = useAuthStore();
+            const newAccessToken = response.data.access;
+            authStore.setUserToken(newAccessToken, refreshToken);
+            originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+            // Reset the refreshing flag before making the retry request
+            refreshing = false;
+
+            return api(originalRequest);
+          } catch (err) {
+            console.log('Refresh token renewal failed:', err);
+            window.location.replace('/');
+            await logout();
+            console.log('Logout successful');
+            alert('다시 로그인 하세요');
+          }
+        } else {
+          console.log('Refresh token renewal is already in progress');
+        }
       }
+      return Promise.reject(error);
     },
   );
 });
-
 export { api };
